fdd37063b4ad242edb72666af0e774860904a5bb
Alex Werner <djwerner@ya.ru>
2016-07-18 13:22:31 +0300

    1807

---
+++ default.php
@@ -0,0 +1,58 @@
+<html><head>
+<link rel="shortcut icon" href="http://100ms.rusff.ru/files/0012/9b/e2/54612.ico" type="image/x-icon" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>100ms.ru</title>
+
+<style type="text/css">
+.transparent {
+           filter: alpha(opacity=85); /*for IE*/
+           opacity: 0.85; /*for Opera, FF*/
+}
+
+body { 
+ background-image: url('http://s2.uploads.ru/F53QI.jpg');
+ background-repeat: no-repeat; 
+ -o-background-size: 100% 100%;
+ -webkit-background-size: 100% 100%;
+ -khtml-background-size: 100% 100%;
+ -moz-background-size: 100% 100%;
+ background-size: 100% 100%;
+ } 
+
+ * html body { 
+filter:progid:DXImageTransform.Microsoft.AlphaImageLoader( src='http://s2.uploads.ru/F53QI.jpg', sizingMethod='scale');
+ background-image: none;
+ }
+ 
+
+  .oval {
+ filter: alpha(opacity=70); /*for IE*/
+         opacity: 0.70; /*for Opera, FF*/
+  text-align:center;
+  height:40px;
+}
+</style>
+</head>
+
+<body>
+
+<table border="0" width="100%" height="100%" cellspacing="5">
+<tr>
+		<td height="100%">&nbsp;<noscript><div class="transparent" style="padding:20px; background-color: #ffffff; color:black; overflow-y: scroll; max-height: 460px;">Мы предлагаем вам Бесплатный Веб и Вап Хостинг, с PHP, MySQL, без рекламы и скрытых платежей.
+Бесплатный хостинг
+Мы предоставляем бесплатный хостинг для организаций образования (школ, вузов, колледжей и других учебных заведений), общественных организаций, коммерческих организаций, индивидуальных предпринимателей и частных лиц.
+Мы бесплатно предоставляем домен третьего уровня you-site.100ms.ru, но вы также можете использовать свой домен любой территориальной зоны, зарегистрированный у любого регистратора доменных имён.
+Если у вас уже есть сайт на другом хостинге, но вы хотели бы перенести сайт на наш бесплатный хостинг, то воспользуйтесь в панели инструментов функциями Импорт сайта, PHPMyAdmin. Виды сайтовВ нашей мощной и легкой Панели управления много профессиональных инструментов для создания блогов, магазинов, социальных сетей и т.д. Наш хостинг поддерживает почти все современные CMS и скрипты.
+Из Панели управления, вы можете работать с Электронной почтой, FTP, Конструктором сайтов; Управлять DNS зоной, Управлять базами MySQL; Создавать субдомены, парковать, перенаправлять домены, создавать бэкапы и др…
+Мы бесплатно включили для вас много профессиональных инструментов для комфортной работы, которые редко встретишь на многих платных хостингах…
+О хостинге:
+Кластерная сеть
+Мы используем мощные кластера веб-серверов, взаимосвязанных друг с другом и работающих как один гигантский компьютер.
+Сочетание мощности сети серверов повышает скорость загрузки, скорость работы, стабильность всей системы в целом, устойчивость к сбоям и атакам. Эта ультрасовременная надежная облачная технология отличает нас от других хостингов.
+</div></noscript></td>
+</tr>
+<tr><td class="oval"><a href="http://100ms.ru/">100ms.ru - Бесплатный Веб и Вап хостинг с PHP, MySQL, без рекламы</a></td></tr>
+</table>
+</body>
+</html>
+<!--DEFAULT_WELCOME_PAGE-->
